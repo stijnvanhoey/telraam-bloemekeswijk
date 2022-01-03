8949833a2f08bfcbc944557c6c2bbc8633ca278d
stijnvanhoey <stijnvanhoey@gmail.com>
2022-01-03 22:57:43 +0100

    Addmdx based blog to website

---
+++ src/routes/blog.svelte
@@ -0,0 +1,37 @@
+<script context="module">
+	// credits to https://github.com/matfantinel/matfantinel.github.io
+
+	export async function load({ fetch }) {
+		const url = '/blog.json';
+		const res = await fetch(url);
+
+		if (res.ok) {
+			return {
+				props: {
+					posts: await res.json()
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load ${url}`)
+		};
+	}
+</script>
+
+<script>
+    import PostCard from '$lib/components/PostCard.svelte';
+
+	export let posts;
+</script>
+
+<div class="row flex-spaces">
+    {#each posts as post}
+        <div class="sm-6 col"><PostCard {post} /></div>
+    {/each}
+  </div>
+
+
+
+
