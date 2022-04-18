72e1253ada368d71bae41cdcc635091f8e01795d
Giorgos Karagounis <giorgos.karagounis@oqton.com>
2022-04-18 18:48:16 +0300

    update and fix package versions

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,6 @@
 /** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ request, resolve }) {
-	const response = await resolve(request, {
+export async function handle({ event, resolve }) {
+	const response = await resolve(event, {
 		ssr: false
 	});
 	return response;
--- src/routes/_utils/_lru_cache.js
+++ src/routes/_utils/_lru_cache.js
@@ -3,11 +3,11 @@ import LRUCache from 'lru-cache';
 const rateLimit = (options) => {
 	const tokenCache = new LRUCache({
 		max: parseInt(options.uniqueTokenPerInterval || 500, 10),
-		maxAge: parseInt(options.interval || 60000, 10)
+		ttl: parseInt(options.interval || 60000, 10)
 	});
 
 	return {
-		check: (request, limit, token) => {
+		check: (event, limit, token) => {
 			const tokenCount = tokenCache.get(token) || [0];
 			if (tokenCount[0] === 0) {
 				tokenCache.set(token, tokenCount);
@@ -15,9 +15,7 @@ const rateLimit = (options) => {
 			tokenCount[0] += 1;
 
 			const currentUsage = tokenCount[0];
-			console.log(
-				`Client ${request.headers['user-agent']} remaining calls: ${limit - currentUsage}`
-			);
+			console.log(`Client ${event.clientAddress} remaining calls: ${limit - currentUsage}`);
 			return currentUsage;
 		}
 	};
